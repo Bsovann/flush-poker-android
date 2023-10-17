a20b74925910056c0c728e382a490bbab581bb75
Bondith Sovann <bsovann04@gmail.com>
2023-10-17 17:05:30 -0500

    Add: challengeButton class

---
+++ app/src/main/java/com/example/flush_poker_android/customviews/ChallengeButton.java
@@ -0,0 +1,33 @@
+package com.example.flush_poker_android.customviews;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.example.flush_poker_android.R;
+
+public class ChallengeButton extends RelativeLayout {
+    public ChallengeButton(Context context) {
+        super(context);
+        init();
+    }
+
+    public ChallengeButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    // Action here
+    private void init() {
+        LayoutInflater.from(getContext()).inflate(R.layout.challenge_button, this, true);
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Challenge Button", "Bondith CLick!");
+            }
+        });
+    }
+}
+
