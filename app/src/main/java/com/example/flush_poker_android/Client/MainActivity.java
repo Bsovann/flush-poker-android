7da923e5a74e16c1bbb971e6a6c33d28cf73029d
Bondith Sovann <bsovann04@gmail.com>
2023-10-16 23:36:19 -0500

    Add: Enable imersive mode

--- app/src/main/java/com/example/flush_poker_android/Client/MainActivity.java
+++ app/src/main/java/com/example/flush_poker_android/Client/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.flush_poker_android.Client;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.flush_poker_android.R;
 
@@ -12,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Enable immersive mode
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 }
