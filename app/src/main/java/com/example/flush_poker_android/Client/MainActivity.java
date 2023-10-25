9b85c70830e927d8cd1a4c3dbde75a56c9146a1b
Bondith Sovann <bsovann04@gmail.com>
2023-10-25 00:52:27 -0500

    Update: setup onChangeBrightnessSeekBar in setting dialog.

--- app/src/main/java/com/example/flush_poker_android/Client/MainActivity.java
+++ app/src/main/java/com/example/flush_poker_android/Client/MainActivity.java
@@ -4,21 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.pm.ActivityInfo;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 
 import com.example.flush_poker_android.R;
 import com.example.flush_poker_android.customviews.ChallengeButton;
 
 public class MainActivity extends AppCompatActivity {
-    private int count = 0;
+
+    private Dialog dialog;
+    private Button closeSettingBtn;
+    private SeekBar brightnessSeekBar;
+    private float screenBrightness = 127 / 255.0f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dialog = new Dialog(this);
 
         // Enable immersive mode
         View decorView = getWindow().getDecorView();
@@ -39,21 +48,52 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Button", "Challenge Button got clicked");
 
     }
+
     public void onClickSettingIcon(View view){
 
         // SettingDialog
-        Dialog settingDialog = new Dialog(this);
-        settingDialog.setContentView(R.layout.setting_dialog);
-        settingDialog.show();
+        dialog.setContentView(R.layout.setting_dialog);
+        brightnessSeekBar = dialog.findViewById(R.id.brightnessSeekBar);
+        closeSettingBtn = dialog.findViewById(R.id.closeSettingButton);
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        brightnessSeekBar.setProgress((int) (screenBrightness * 255));
+        dialog.show();
+
+        // Brightness control
+        setupSeekBarListener();
 
         // Exit button
-        Button closeSettingBtn = settingDialog.findViewById(R.id.closeSettingButton);
-        closeSettingBtn.setOnClickListener(x -> settingDialog.dismiss());
-
+        closeSettingBtn = dialog.findViewById(R.id.closeSettingButton);
+        closeSettingBtn.setOnClickListener(x -> dialog.dismiss());
     }
 
     public void onClickInstructionIcon(View view){
         RelativeLayout instructionIcon = findViewById(R.id.instructionIcon);
         Log.i("Icon", "Instruction Icon got clicked");
+    }
+
+    public void setupSeekBarListener(){
+        brightnessSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int currentValue, boolean fromUser) {
+                if(fromUser){
+                    screenBrightness = currentValue / 255.0f;
+                    WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+                    layoutParams.screenBrightness = screenBrightness;
+                    getWindow().setAttributes(layoutParams);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
