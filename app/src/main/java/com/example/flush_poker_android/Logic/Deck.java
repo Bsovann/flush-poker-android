ca91d718e125203ad2821a39509bec7b103418a1
Bondith Sovann <bsovann04@gmail.com>
2023-10-27 23:00:22 -0500

    Add: standard cards and deck

---
+++ app/src/main/java/com/example/flush_poker_android/Logic/Deck.java
@@ -0,0 +1,32 @@
+package com.example.flush_poker_android.Logic;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+    private List<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                cards.add(new Card(rank, suit));
+            }
+        }
+        shuffle();
+    }
+
+    private void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card dealCard() {
+        if (cards.isEmpty()) {
+            // Handle out of cards situation
+            return null;
+        }
+        return cards.remove(0);
+    }
+}
+
