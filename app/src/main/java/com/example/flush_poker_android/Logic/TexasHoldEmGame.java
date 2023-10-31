37ba17c6dce30a8b4b058342d4e8665013490360
Bondith Sovann <bsovann04@gmail.com>
2023-10-31 14:59:16 -0500

    Update: setup game.

--- app/src/main/java/com/example/flush_poker_android/Logic/TexasHoldEmGame.java
+++ app/src/main/java/com/example/flush_poker_android/Logic/TexasHoldEmGame.java
@@ -1,5 +1,42 @@
 package com.example.flush_poker_android.Logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TexasHoldEmGame {
+    private Deck deck;
+    private List<Player> players;
+    private List<Card> communityCards;
+    private List<Player> bettingRound;
+    private int dealerPosition;
+    private int pot;
+
+
+
+    public TexasHoldEmGame(){
+        this.deck = new Deck();
+        this.players = new ArrayList<>(5);
+        this.communityCards = new ArrayList<>();
+        this.dealerPosition = 0;
+    }
+
+    public void addPlayer(Player player){
+        this.players.add(player);
+    }
+
+    public void dealCommunityCards(){
+        communityCards.add(deck.dealCard());
+    }
+
+    public List getCommunityCards(){
+        return communityCards;
+    }
+
+    public int getDealerPosition(){
+        return this.dealerPosition;
+    }
+    public void incrementDealerPosition(){
+        this.dealerPosition++;
+    }
 
 }
