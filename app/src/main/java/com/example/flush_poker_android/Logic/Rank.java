de58f6eab8d9e52846bd9490011363f1ead95617
Bondith Sovann <bsovann04@gmail.com>
2023-11-07 21:01:15 -0600

    Rank: number to word

--- app/src/main/java/com/example/flush_poker_android/Logic/Rank.java
+++ app/src/main/java/com/example/flush_poker_android/Logic/Rank.java
@@ -5,15 +5,15 @@ public enum Rank {
     KING("King", 13),
     QUEEN("Queen", 12),
     JACK("Jack", 11),
-    TEN("10", 10),
-    NINE("9", 9),
-    EIGHT("8", 8),
-    SEVEN("7", 7),
-    SIX("6", 6),
-    FIVE("5", 5),
-    FOUR("4", 4),
-    THREE("3", 3),
-    TWO("2", 2);
+    TEN("Ten", 10),
+    NINE("Nine", 9),
+    EIGHT("Eight", 8),
+    SEVEN("Seven", 7),
+    SIX("Six", 6),
+    FIVE("Five", 5),
+    FOUR("Four", 4),
+    THREE("Three", 3),
+    TWO("Two", 2);
 
     private final String name;
     private final int value;
