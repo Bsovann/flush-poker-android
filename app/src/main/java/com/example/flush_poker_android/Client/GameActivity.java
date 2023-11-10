0a71b0b3ee58c373d420d25ac72191f67b5e0ea7
Bsovann <bsovann04@gmail.com>
2023-11-10 02:00:54 -0600

    Add: GameLoop and BetTurner mechanism.

--- app/src/main/java/com/example/flush_poker_android/Client/GameActivity.java
+++ app/src/main/java/com/example/flush_poker_android/Client/GameActivity.java
@@ -2,16 +2,23 @@ package com.example.flush_poker_android.Client;
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.example.flush_poker_android.Client.customviews.CardAdapter;
+import com.example.flush_poker_android.Logic.BettingLogic;
+import com.example.flush_poker_android.Logic.TexasHoldEmGamePracticeMode;
 import com.example.flush_poker_android.R;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -28,6 +35,15 @@ public class GameActivity extends AppCompatActivity {
 
     List<CardAdapter> playerAdapter;
     CardAdapter commnityCardAdapter;
+
+    private TextView timerTextView;
+    private BettingLogic bettingLogic;
+    private CountDownTimer countDownTimer;
+    private boolean isRunning = false;
+    private final Handler handler = new Handler(Looper.getMainLooper());
+
+    private TexasHoldEmGamePracticeMode gameLogic;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,12 +51,51 @@ public class GameActivity extends AppCompatActivity {
 
         initWork();
 
+        gameLogic.startPlayerTurn();
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        this.isRunning = false;
+        gameLoop();
+    }
 
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        gameLogic.unregisterReceiver(this);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        isRunning = false;
+    }
+
+    private void gameLoop() {
+        if (isRunning) {
+            startTimer(30000); // 30 seconds
+            // Handle user input (e.g., touch events) to make player moves
+            //pokerGameView.handleInput();
+//
+//            // Update the game state
+//            pokerGameLogic.update();
+//
+//            // Render the game view
+//            pokerGameView.invalidate(); // This will trigger a redraw of the game view
+            Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
+
+            // Define your desired frame rate (e.g., 30 FPS)
+            int frameRate = 1000 / 30;
+
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    gameLoop(); // Call the game loop again after a delay
+                }
+            }, frameRate);
+        }
     }
 
     private void initWork(){
@@ -85,6 +140,14 @@ public class GameActivity extends AppCompatActivity {
         Collections.reverse(communityCardImages);
         commnityCardAdapter = new CardAdapter(this, communityCardImages);
         communityCardView.setAdapter(commnityCardAdapter);
+
+        //
+        timerTextView = findViewById(R.id.timerTextView); // Add a TextView for the timer
+        // Start the countdown timer with a 30-second limit (adjust as needed)
+
+        gameLogic = new TexasHoldEmGamePracticeMode(this, handler);
+
+
     }
 
     public void onClickExitBtn(View view){
@@ -94,19 +157,53 @@ public class GameActivity extends AppCompatActivity {
 
     public void onClickFoldBtn(View view){
         Toast.makeText(this, "Fold Btn got Clicked!", Toast.LENGTH_SHORT).show();
+        handlePlayerAction("Fold");
     }
 
     public void onClickCheckBtn(View view){
         Toast.makeText(this, "Check Btn got Clicked!", Toast.LENGTH_SHORT).show();
+        handlePlayerAction("Check");
     }
 
     public void onClickCallBtn(View view){
         Toast.makeText(this, "Call Btn got Clicked!", Toast.LENGTH_SHORT).show();
+        handlePlayerAction("Call");
     }
 
     public void onClickBetBtn(View view){
         Toast.makeText(this, "Bet Btn got Clicked!", Toast.LENGTH_SHORT).show();
+        handlePlayerAction("Bet");
     }
+
+    private void handlePlayerAction(String action) {
+        Intent intent = new Intent("com.example.flush_poker_android.ACTION");
+        intent.putExtra("action", action);
+
+        // Start the service or broadcast the intent
+        sendBroadcast(intent);
+    }
+
+    private void startTimer(long milliseconds) {
+        countDownTimer = new CountDownTimer(milliseconds, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timerTextView.setText("Time remaining: " + millisUntilFinished / 1000 + " seconds");
+            }
+            @Override
+            public void onFinish() {
+                // Perform actions when the timer finishes (e.g., force the player to fold)
+                handlePlayerAction("Fold");
+            }
+        }.start();
+    }
+
+    private void stopTimer() {
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+            timerTextView.setText("Timer stopped");
+        }
+    }
+
 
     public void onClickChatIcon(View view){
         // Chat Dialog
