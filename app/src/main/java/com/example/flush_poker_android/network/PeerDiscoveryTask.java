6f39543682a130de41acde4442d00754394552bb
Kyle Chainey <kylechainey99@gmail.com>
2023-11-13 21:21:31 -0600

    Adding PeerDiscovery AsyncTask

---
+++ app/src/main/java/com/example/flush_poker_android/network/PeerDiscoveryTask.java
@@ -0,0 +1,10 @@
+package com.example.flush_poker_android.network;
+
+import android.os.AsyncTask;
+
+public class PeerDiscoveryTask extends AsyncTask<Void, Void, Boolean> {
+    @Override
+    protected Boolean doInBackground(Params... params) {
+        return null;
+    }
+}
