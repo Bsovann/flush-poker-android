f1009e1c31f75e67392185253c90e4b61bc87ad3
Bsovann <bsovann04@gmail.com>
2023-11-20 18:20:19 -0600

    Remove: log.d.

--- app/src/main/java/com/example/flush_poker_android/Client/Utility/CardUtils.java
+++ app/src/main/java/com/example/flush_poker_android/Client/Utility/CardUtils.java
@@ -1,22 +1,12 @@
 package com.example.flush_poker_android.Client.Utility;
 
 import android.content.Context;
-import android.util.Log;
 
 public class CardUtils {
     public static int getCardImageResourceId(String cardName, Context context) {
-        // Add debug logging
-        Log.d("CardUtils", "getCardImageResourceId: cardName = " + cardName + ", context = " + context);
-
-        // Assuming your card names are in the format 'ace_of_clubs'
-        // Convert the card name to lowercase and replace underscores with lowercase letters
         String resourceName = cardName;
-
         // Get the resource ID dynamically using the generated R.drawable class
         int resourceId = context.getResources().getIdentifier(resourceName, "drawable", context.getPackageName());
-
-        // Add debug logging
-        Log.d("CardUtils", "getCardImageResourceId: resourceId = " + resourceId);
 
         return resourceId;
     }
