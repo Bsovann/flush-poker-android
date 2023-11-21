709190be99063d70d11b65f03157d395b9adf4e6
Bsovann <bsovann04@gmail.com>
2023-11-20 18:33:46 -0600

    Add: HandleMessage, RenderNewUI, handleMessage THread,& on cardUpdate.

--- app/src/main/java/com/example/flush_poker_android/Client/HostActivity.java
+++ app/src/main/java/com/example/flush_poker_android/Client/HostActivity.java
@@ -7,6 +7,8 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -15,6 +17,7 @@ import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.flush_poker_android.Client.Utility.GameInfo;
 import com.example.flush_poker_android.Client.customviews.CardAdapter;
 import com.example.flush_poker_android.Logic.BotPlayer;
 import com.example.flush_poker_android.Logic.GameController;
@@ -22,13 +25,14 @@ import com.example.flush_poker_android.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
-public class HostActivity extends AppCompatActivity {
+public class HostActivity extends AppCompatActivity implements GameUpdateListener {
     private Dialog dialog;
     private SeekBar brightnessSeekBar;
     private float screenBrightness = 127 / 255.0f;
@@ -36,39 +40,72 @@ public class HostActivity extends AppCompatActivity {
     List<GridView> playersView;
     List<CardAdapter> playerAdapter;
     CardAdapter commnityCardAdapter;
-    private final Handler handler = new Handler(Looper.getMainLooper());
+    private Handler handler = new Handler(Looper.getMainLooper());
     private List<BotPlayer> players;
     private int pot;
     private Thread controllerThread;
     private ExecutorService playerThreadPool;
     private GameController gameController;
     private CardFragment cardFragment;
+    private Set<Integer> communityCardIds = new HashSet<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        initWork();
-
-        // Create CardFragment instance
-        cardFragment = new CardFragment();
-
-        // Check if the fragment is already added
+        // Check if the fragment is not already added to avoid duplication
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_card, cardFragment) // R.id.fragment_container is the ID of a FrameLayout in your activity's layout
-                    .commit();
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_card, CardFragment.class, null)
+                    .commitNow();
         }
+        initWork();
 
+    }
+    private long lastMessageTimestamp;
+    public void handleMessage() {
+        // Create a handler for the UI thread
+        handler = new Handler(getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                // Process data received from the background thread
+                if (msg.what == 1) {
+                    Bundle bundle = msg.getData();
+                    GameInfo dataObject = (GameInfo) bundle.getSerializable("dataObject");
+                    if (dataObject != null) {
+                        // Check if the message is new based on its timestamp
+                        // Process the new data and call onResume
+                        communityCardIds.addAll(dataObject.getCommunityCardIds());
+                        onResume();
 
-
-
+                        Log.i("HandleMessage", communityCardIds.toString());
+                    }
+                }
+            }
+        };
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        cardFragment = (CardFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_card);
+//      handleMessage();
+        renderNewUI();
+    }
+
+    private void renderNewUI(){
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                onCardUpdate(communityCardIds.stream().collect(Collectors.toList()));
+                // update playerTurn position
+                // update current pot
+                // update dealer position
+                // update the winner
+            }
+        });
     }
     @Override
     protected void onDestroy(){
@@ -94,14 +131,15 @@ public class HostActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
         setContentView(R.layout.activity_game);
 
+        // Set up work
         dialog = new Dialog(this);
-
         playersView = new ArrayList<>(5);
-        communityCardView = findViewById(R.id.communityCardGrid);
         playerAdapter = new ArrayList<>(5);
         players = new ArrayList<>();
 
+        // Default info rendering
         renderImagesTemp();
+
         this.playerThreadPool = Executors.newFixedThreadPool(5);
         // Assign Each player to each Thread
         for(int i = 0; i < 5; i++){
@@ -109,19 +147,26 @@ public class HostActivity extends AppCompatActivity {
                     "Player "+ i, 50000, handler, getApplicationContext()));
         }
 
-        this.gameController = new GameController(players, handler,getApplicationContext(), playerThreadPool);
+        // Listening to GameController if anything update.
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(() -> {
+            //Background work here
+            handleMessage();
+        });
 
-        // Add CardFragment as a listener to the GameController
-        gameController.addGameUpdateListener(cardFragment);
-
+        // Instantiate GameController
+        this.gameController = new GameController(players, handler, getApplicationContext(), playerThreadPool);
 
         // Set Controller to every player and launch Thread
         for(BotPlayer player : players) {
             player.setController(gameController);
             playerThreadPool.submit(player);
         }
+
+        // Instantiate controllerThread and start it.
         this.controllerThread = new Thread(gameController);
         controllerThread.start();
+
     }
     private void renderImagesTemp(){
         playersView.add(findViewById(R.id.myCards));
@@ -135,21 +180,10 @@ public class HostActivity extends AppCompatActivity {
             playerAdapter.add(new CardAdapter(this, Arrays.asList(R.drawable.back_of_card1, R.drawable.back_of_card1)));
         }
 
-
         // Render card
         for(int i = 0; i < 5; i++) {
             playersView.get(i).setAdapter(playerAdapter.get(i));
         }
-        List<Integer> communityCardImages = new LinkedList<>();
-        // Add Community cards images
-        communityCardImages.add(R.drawable.back_of_card);
-        communityCardImages.add(R.drawable.back_of_card);
-        communityCardImages.add(R.drawable.back_of_card);
-        communityCardImages.add(R.drawable.back_of_card);
-        communityCardImages.add(R.drawable.back_of_card);
-        Collections.reverse(communityCardImages);
-        commnityCardAdapter = new CardAdapter(this, communityCardImages);
-        communityCardView.setAdapter(commnityCardAdapter);
     }
     public void onClickExitBtn(View view){
         Intent intent = new Intent(HostActivity.this, MainActivity.class);
@@ -233,5 +267,14 @@ public class HostActivity extends AppCompatActivity {
 
             }
         });
+    }
+    @Override
+    public void onCardUpdate(List<Integer> updatedCardImages) {
+        if (cardFragment != null) {
+            cardFragment.onCardUpdate(updatedCardImages);
+        } else {
+            // Handle the case where cardFragment is null
+            Log.e("YourActivity", "cardFragment is null");
+        }
     }
 }
